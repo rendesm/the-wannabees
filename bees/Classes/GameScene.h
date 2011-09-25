9d752ef7fb59c446522c401585e221ec8fe871bb
Mihaly Rendes <micheal@freemail.hu>
2011-09-25 19:03:57 +0200

    home

---
+++ bees/Classes/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  bees
+//
+//  Created by Mihaly Rendes on 9/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol GameScene <NSObject>
+
+@end
